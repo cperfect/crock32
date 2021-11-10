addfc39be9b91f13dc9c93a4335a5dbdfc390139
Chris Perfect <1636270+cperfect@users.noreply.github.com>
2021-11-10 11:35:56 +1100

    Fix lint issues

--- src/chunks.ts
+++ src/chunks.ts
@@ -1,5 +1,7 @@
 // masks for bitwise copying
-export const getCopyMask = (start: number, end: number, bitLength: number): string => {
+export const getCopyMask = (
+    start: number, end: number, bitLength: number,
+): string => {
   if (start < 1 || start > bitLength || end <= start || end > bitLength) {
     throw new Error(`Invalid start & end : ${start} & ${end}`);
   }
@@ -13,13 +15,13 @@ export const getCopyMask = (start: number, end: number, bitLength: number): stri
       mask = mask.concat('0');
     }
   }
-  //return Number.parseInt(mask, 2);
+  // return Number.parseInt(mask, 2);
   return mask;
-}
+};
 
 const sigBits = (val: number): number => {
   return Math.floor(Math.log(val)/Math.log(2))+1;
-}
+};
 
 const countTrailingZeros = (val: number) => {
   let count = 0;
@@ -27,25 +29,31 @@ const countTrailingZeros = (val: number) => {
     const notAZero = val % Math.pow(2, i);
     if (notAZero) {
       break;
-    } 
+    }
     count++;
   }
   return count;
-}
+};
 
 export type PartialChunk = {
   bits: number;
   length: number;
 }
 
-export const combinePartials = (left: PartialChunk, right: PartialChunk): number => {
+export const combinePartials = (
+    left: PartialChunk,
+    right: PartialChunk,
+): number => {
   if (left.length + right.length != 5) {
+    // eslint-disable-next-line max-len
     throw new Error(`Combined length of partial chunks must be 5 (${left.length} + ${right.length} != 5)`);
   }
-  const lVal = sigBits(left.bits) === left.length ? left.bits << 5 - left.length : left.bits << sigBits(left.bits);
-  const rVal = sigBits(right.bits) === right.length ? right.bits >>> 5 - right.length : right.bits >>> sigBits(right.bits);
-  //const lVal = left.bits << left.length;
-  //const rVal = right.bits >>> right.length;
+  const lVal = sigBits(left.bits) === left.length ?
+  left.bits << 5 - left.length : left.bits << sigBits(left.bits);
+  const rVal = sigBits(right.bits) === right.length ?
+  right.bits >>> 5 - right.length : right.bits >>> sigBits(right.bits);
+  // const lVal = left.bits << left.length;
+  // const rVal = right.bits >>> right.length;
   return lVal + rVal;
 };
 
@@ -55,39 +63,44 @@ export const combinePartialsOld = (left: number, right: number): number => {
   const ltz = countTrailingZeros(left);
   // const rtz = countTrailingZeros(right);
   const rightShift = sigBits(right) <= ltz ? 0 : 5 - ltz;
+  // eslint-disable-next-line max-len
   // const rightShift = sigBits(right) < leftShift  ? leftShift - sigBits(right) : 0;
-  //const tz = countTrailingZeros(left);
-  //const rightShift = sigBits(right) > leftShift ? sigBits(right) - leftShift : 0;
-  //const rightShift = leftShift ? 5 - leftShift : 0;
-  //const rightShift = sigBits(left) + sigBits() >? 
-  //const rightShift = countTrailingZeros(right);
-  //const rightShift = leftShift ? sigBits(right) - leftShift : 0;
-  //const rightShift = (5 - leftShift) - sigBits(right);
+  // const tz = countTrailingZeros(left);
+  // eslint-disable-next-line max-len
+  // const rightShift = sigBits(right) > leftShift ? sigBits(right) - leftShift : 0;
+  // const rightShift = leftShift ? 5 - leftShift : 0;
+  // const rightShift = sigBits(left) + sigBits() >?
+  // const rightShift = countTrailingZeros(right);
+  // const rightShift = leftShift ? sigBits(right) - leftShift : 0;
+  // const rightShift = (5 - leftShift) - sigBits(right);
   right = right >>> rightShift;
   const chunk = left + right;
-  if (chunk > 0b11111) { 
+  if (chunk > 0b11111) {
+    // eslint-disable-next-line max-len
     throw new Error(`Invalid partials: ${left} + ${right} greater than 31 (0b11111)`);
   }
   return chunk;
-}
+};
 
 export const byteToChunk = (val: number, mask: string): number => {
   const maskNum = Number.parseInt(mask, 2);
-  const trailingZeroIndex = mask.replace(/^0+/, '-').indexOf('0'); // we will get -1 if no trailing zeros
+  const trailingZeroIndex = mask.replace(/^0+/, '-')
+      .indexOf('0'); // we will get -1 if no trailing zeros
   const shift = trailingZeroIndex !== -1 ? 8 - trailingZeroIndex: 0;
-  let bits = val & maskNum;
+  const bits = val & maskNum;
   return bits >> shift;
-}
+};
 
 export const toChunks = (uint8: Uint8Array): number[] => {
   const chunks = [] as number[];
-  let startChunk = 1; //1 index on a byte
-  let endChunk = 5; 
+  let startChunk = 1; // 1 index on a byte
+  let endChunk = 5;
   let partialChunk: PartialChunk | null = null;
   uint8.forEach((byte, idx) => {
     if (endChunk < 5) { // we have a 'right' partial at the front
       const mask = getCopyMask(startChunk, endChunk, 8);
       if (!partialChunk) {
+        // eslint-disable-next-line max-len
         throw new Error(`Found right partial without left partial in byte ${idx}`);
       }
       const chunk = combinePartials(partialChunk, {
@@ -95,25 +108,28 @@ export const toChunks = (uint8: Uint8Array): number[] => {
         length: 5 - (endChunk - startChunk),
       });
       chunks.push(chunk);
+      // eslint-disable-next-line max-len
       // chunks[chunks.length-1] = combinePartialsOld(chunks[chunks.length-1], partialChunk);
       startChunk = endChunk + 1;
-      endChunk = (startChunk + 5) <= 8 ? (startChunk + 5) : 8 ;
+      endChunk = (startChunk + 5) <= 8 ? (startChunk + 5) : 8;
     }
 
     if (startChunk + 5 <= 8) {
-      let mask = getCopyMask(startChunk, endChunk, 8);
+      const mask = getCopyMask(startChunk, endChunk, 8);
       chunks.push(byteToChunk(byte, mask));
       startChunk = endChunk + 1;
       endChunk = (startChunk + 5) < 8 ? (startChunk + 5) : 8;
     }
 
     if (startChunk < 8) { // we have a 'left' partial
-      let mask = getCopyMask(startChunk, endChunk, 8);
-      if (idx == uint8.length - 1) { // we are out of bytes so pad this chunk out
+      const mask = getCopyMask(startChunk, endChunk, 8);
+      if (idx == uint8.length - 1) {
+        // we are out of bytes so pad this chunk out
         let chunk = byteToChunk(byte, mask);
-        chunk = byteToChunk(chunk, getCopyMask(0, sigBits(chunk)-1, 8))
+        chunk = byteToChunk(chunk, getCopyMask(0, sigBits(chunk)-1, 8));
         chunks.push(chunk);
-        //right pad the last chunk with 0 if necessary
+        // right pad the last chunk with 0 if necessary
+        // eslint-disable-next-line max-len
         // partialChunk = byteToChunk(partialChunk, getCopyMask(0, sigBits(partialChunk)-1, 8));//tehnically a complete chunk
       } else { // we expect to find the 'right' partial in the next byte
         partialChunk = {
@@ -121,7 +137,7 @@ export const toChunks = (uint8: Uint8Array): number[] => {
           length: 5 - (endChunk - startChunk),
         };
       }
-      //chunks.push(partialChunk);
+      // chunks.push(partialChunk);
       endChunk = (startChunk + 5) % 8;
       startChunk = 1;
     }
--- test/chunks.test.ts
+++ test/chunks.test.ts
@@ -1,73 +1,75 @@
-import { expect } from 'chai';
-import { combinePartialsOld, byteToChunk, getCopyMask, toChunks, combinePartials } from '../src/chunks';
+import {expect} from 'chai';
+import {
+  combinePartialsOld, byteToChunk, getCopyMask, toChunks, combinePartials,
+} from '../src/chunks';
 
 
 describe('Get Copy Mask', () => {
   it('1,3 on 8 bits => 11100000', () => {
-  const m = getCopyMask(1,3,8);
-  expect(m).to.equal('11100000');
- });
- it('1,5 on 8 bits => 11111000', () => {
-  const m = getCopyMask(1,5,8);
-  expect(m).to.equal('11111000');
- });
- it('3,7 on 8 bits => 00111110', () => {
-  const m = getCopyMask(3,7,8);
-  expect(m).to.equal('00111110');
- });
- it('2,6 on 8 bits => 01111100', () => {
-  const m = getCopyMask(2,6,8);
-  expect(m).to.equal('01111100');
- });
- it('5,8 on 8 bits => 00001111', () => {
-  const m = getCopyMask(5,8,8);
-  expect(m).to.equal('00001111');
- });
- it('2,5 on 5 bits => 01111', () => {
-  const m = getCopyMask(2,5,5);
-  expect(m).to.equal('01111');
- });
+    const m = getCopyMask(1, 3, 8);
+    expect(m).to.equal('11100000');
+  });
+  it('1,5 on 8 bits => 11111000', () => {
+    const m = getCopyMask(1, 5, 8);
+    expect(m).to.equal('11111000');
+  });
+  it('3,7 on 8 bits => 00111110', () => {
+    const m = getCopyMask(3, 7, 8);
+    expect(m).to.equal('00111110');
+  });
+  it('2,6 on 8 bits => 01111100', () => {
+    const m = getCopyMask(2, 6, 8);
+    expect(m).to.equal('01111100');
+  });
+  it('5,8 on 8 bits => 00001111', () => {
+    const m = getCopyMask(5, 8, 8);
+    expect(m).to.equal('00001111');
+  });
+  it('2,5 on 5 bits => 01111', () => {
+    const m = getCopyMask(2, 5, 5);
+    expect(m).to.equal('01111');
+  });
 });
 
 xdescribe('Old Combine Partials', () => {
   it('0b11100, 0b00011 => 0b11111', () => {
-    const c = combinePartialsOld(0b11100, 0b00011); //r 0, l 0
+    const c = combinePartialsOld(0b11100, 0b00011); // r 0, l 0
     expect(c).to.equal(0b11111);
   });
   it('0b11000, 0b00111 => 0b11111', () => {
-    const c = combinePartialsOld(0b11000, 0b00111); //r 0, l 0
+    const c = combinePartialsOld(0b11000, 0b00111); // r 0, l 0
     expect(c).to.equal(0b11111);
   });
   it('0b11000, 0b00011 => 0b11011', () => {
-    const c = combinePartialsOld(0b11000, 0b00011); //r 0, l 0
+    const c = combinePartialsOld(0b11000, 0b00011); // r 0, l 0
     expect(c).to.equal(0b11011);
   });
   it('0b01000, 0b00011 => 0b01011', () => {
-    const c = combinePartialsOld(0b01000, 0b00011); //r 0, l 0
+    const c = combinePartialsOld(0b01000, 0b00011); // r 0, l 0
     expect(c).to.equal(0b01011);
   });
   it('0b00010, 0b00111 => 0b10111', () => {
-    const c = combinePartialsOld(0b00010, 0b00111); //r 3, l 0
+    const c = combinePartialsOld(0b00010, 0b00111); // r 3, l 0
     expect(c).to.equal(0b10111);
   });
   it('0b00111, 0b00010 => 0b10111', () => {
-    const c = combinePartialsOld(0b00111, 0b00010); //r 2, l 0
+    const c = combinePartialsOld(0b00111, 0b00010); // r 2, l 0
     expect(c).to.equal(0b11110);
   });
   it('0b00111, 0b11000 => 0b11111', () => {
-    const c = combinePartialsOld(0b00111, 0b11000); //r 2, l 3
+    const c = combinePartialsOld(0b00111, 0b11000); // r 2, l 3
     expect(c).to.equal(0b11111);
   });
   it('0b00111, 0b01000 => 0b11101', () => {
-    const c = combinePartialsOld(0b00111, 0b01000); //r 2, l 3
+    const c = combinePartialsOld(0b00111, 0b01000); // r 2, l 3
     expect(c).to.equal(0b11101);
   });
   it('0b00111, 0b10000 => 0b11110', () => {
-    const c = combinePartialsOld(0b00111, 0b10000); //r 2, l 3
+    const c = combinePartialsOld(0b00111, 0b10000); // r 2, l 3
     expect(c).to.equal(0b11110);
   });
   it('0b01101, 0b10000 => 0b11011', () => {
-    const c = combinePartialsOld(0b01101, 0b10000); //r 1, l 4
+    const c = combinePartialsOld(0b01101, 0b10000); // r 1, l 4
     expect(c).to.equal(0b11011);
   });
   it('0b01101, 0b01000 => error (greater than 31)', () => {
@@ -84,10 +86,10 @@ describe('Combine Partials', () => {
       bits: 0b11100,
       length: 0,
     },
-    { 
+    {
       bits: 0b00011,
       length: 0,
-    }); //r 0, l 0
+    }); // r 0, l 0
     expect(c).to.equal(0b11111);
   });
   it('0b111 > 2, 0b11 < 0 => 0b11111', () => {
@@ -95,10 +97,10 @@ describe('Combine Partials', () => {
       bits: 0b111,
       length: 2,
     },
-    { 
+    {
       bits: 0b11,
       length: 0,
-    }); //r 0, l 0
+    }); // r 0, l 0
     expect(c).to.equal(0b11111);
   });
   it('0b10 > 3, 0b111 < 0 => 0b10111', () => {
@@ -106,34 +108,33 @@ describe('Combine Partials', () => {
       bits: 0b10,
       length: 3,
     },
-    { 
+    {
       bits: 0b111,
       length: 0,
-    }); //r 0, l 0
+    }); // r 0, l 0
     expect(c).to.equal(0b10111);
   });
- 
 });
 
 describe('Byte to Chunk', () => {
   it('0b11100000, 1,3 => 0b111', () => {
-    const b = byteToChunk(0b11100000, getCopyMask(1,3,8));
+    const b = byteToChunk(0b11100000, getCopyMask(1, 3, 8));
     expect(b).to.equal(0b111);
   });
   it('0b00111000, 2,6 => 0b1110', () => {
-    const b = byteToChunk(0b00111000, getCopyMask(2,6,8));
+    const b = byteToChunk(0b00111000, getCopyMask(2, 6, 8));
     expect(b).to.equal(0b01110);
   });
   it('0b00001111, 1,3 => 0b000', () => {
-    const b = byteToChunk(0b00001111, getCopyMask(1,3,8));
+    const b = byteToChunk(0b00001111, getCopyMask(1, 3, 8));
     expect(b).to.equal(0b000);
   });
   it('0b00000000, 1,3 => 0b000', () => {
-    const b = byteToChunk(0b00000000, getCopyMask(1,3,8));
+    const b = byteToChunk(0b00000000, getCopyMask(1, 3, 8));
     expect(b).to.equal(0b000);
   });
   it('0b00000110, 5,8 => 0b110', () => {
-    const b = byteToChunk(0b00000110, getCopyMask(5,8,8));
+    const b = byteToChunk(0b00000110, getCopyMask(5, 8, 8));
     expect(b).to.equal(0b110);
   });
 });
@@ -151,7 +152,7 @@ describe('To Chunks', () => {
       0b01011,
       0b11111,
       0b11011,
-      0b00000, //padded with 1 trailing zero
+      0b00000, // padded with 1 trailing zero
     ]);
   });
 });
